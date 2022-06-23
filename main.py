04405de7ae9543138dd02601515e1600fd58abbb
Furknn <fs.2838@gmail.com>
2022-06-23 12:07:40 +0300

    graph generation, project pdf

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def solve_greedy(path):
         mip.Solve()
 
 
-def solve_genetic(path, generations: list, popSizes: list):
+def solve_genetic(path, generations: list[int], popSizes: list[int]):
     # Solve all graphs under folder using genetic solver
     # can be used like: solve_genetic("./Generated_Graphs", [50, 100, 150], [50, 100])
     graph_dirs = os.listdir(path=path)
@@ -41,8 +41,17 @@ def solve_genetic(path, generations: list, popSizes: list):
                 mip.Solve()
 
 
+def generate_graphs(node_counts: list[int], densities: list[float]):
+    # Generate graphs and saves them under "./Generated_Graphs"
+    # can be used like: generate_graphs([500, 1000, 1500, 2000], [0.1, 0.3, 0.5, 0.7, 0.9])
+    for node_count in node_counts:
+        for density in densities:
+            Graph(node_count, density)
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    generate_graphs([500, 1000, 1500, 2000], [0.1, 0.3, 0.5, 0.7, 0.9])
     solve_mips("/.Generated_Graphs")
     solve_greedy("/.Generated_Graphs")
     solve_genetic("./Generated_Graphs", [50, 100, 150], [50, 100])
